c301eb50d4e1ed5d338f6df0d6830b0f73e26c32
Anna Silinskaya <annsilin@mail.ru>
2026-03-21 19:00:09 +0400

    fix: conftest clients imports

--- conftest.py
+++ conftest.py
@@ -10,6 +10,10 @@ import os
 import pytest
 
 from api.users_client import UsersClient
+from api.comments_client import CommentsClient
+from api.health_client import HealthClient
+from api.posts_client import PostsClient
+from api.todos_client import TodosClient
 from config.config import ALLURE_RESULTS_DIR, BASE_URL
 
 logging.basicConfig(
@@ -23,6 +27,22 @@ logger = logging.getLogger("conftest")
 @pytest.fixture(scope="session")
 def users_client() -> UsersClient:
     return UsersClient(BASE_URL)
+
+@pytest.fixture(scope="session")
+def posts_client() -> PostsClient:
+    return PostsClient(BASE_URL)
+
+@pytest.fixture(scope="session")
+def todos_client() -> TodosClient:
+    return TodosClient(BASE_URL)
+
+@pytest.fixture(scope="session")
+def comments_client() -> CommentsClient:
+    return CommentsClient(BASE_URL)
+
+@pytest.fixture(scope="session")
+def health_client() -> HealthClient:
+    return HealthClient(BASE_URL)
 
 @pytest.fixture(scope="session", autouse=True)
 def _setup_dirs():
